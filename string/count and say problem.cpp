26dbc705ea256ba00ad24029cf4802b61d970341
Mohit singh <mohitrajputms123@gmail.com>
2021-08-24 10:11:24 +0530

    Create count and say problem.cpp

---
+++ string/count and say problem.cpp
@@ -0,0 +1,21 @@
+
+string countnsay(int n) {
+    if(n==1) return "1";
+    else if(n==2) return "11";
+    string s="11";
+    for(int i=3;i<=n;i++){
+        string t="";
+        int c=1;
+        s+='$';
+        for(int j=1;j<s.length();j++){
+            if(s[j]!=s[j-1]){
+                t+=to_string(c);
+                t+=s[j-1];
+                c=1;
+            } 
+            else c++;
+        }
+        s=t;
+    }
+    return s;
+}
