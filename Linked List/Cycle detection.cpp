87508a0e90f4f7a3b38e474acd13d8650fb809a9
Mohit singh <mohitrajputms123@gmail.com>
2021-10-13 11:06:31 +0530

    Create Cycle detection.cpp

---
+++ Linked List/Cycle detection.cpp
@@ -0,0 +1,183 @@
+// Author Mohit
+// Implementation of Singly linked list
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+    
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Linkedlist {
+    Node* head;
+public:
+    Linkedlist() {
+        head = NULL;
+    }
+    
+    void insertBegin(int);
+    
+    void insertEnd(int);
+    
+    void insertAfter(Node*, int);
+    
+    int deleteBegin();
+    
+    int deleteEnd();
+    
+    int deleteAfter(Node*);
+    
+    void printList();
+    
+    void reverseList();
+    
+    void reverseinK(Node*, int);
+    
+    bool cycledetection();
+    
+};
+
+void Linkedlist :: insertBegin(int data) {
+    Node* temp = new Node(data);
+    if(head == NULL) {
+        head = temp;
+    } else {
+        temp->next = head;
+        head = temp;
+    }
+}
+
+void Linkedlist :: insertEnd(int data) {
+    Node* temp = new Node(data);
+    if(head == NULL){
+        head = temp;
+    } else {
+        Node* curr = head;
+        while(curr->next!=NULL) curr = curr->next;
+        curr->next = temp;
+    }
+}
+
+void Linkedlist :: insertAfter(Node* prev,int data) {
+    Node* temp = new Node(data);
+    if(prev != NULL) {
+        prev->next = temp;
+    }
+}
+
+int Linkedlist :: deleteBegin() {
+    Node* temp = head;
+    if(head == NULL || head->next == NULL) {
+        return 0;
+    } else {
+        int res = head->data;
+        head = head->next;
+        delete temp;
+        return res;
+    }
+}
+
+int Linkedlist :: deleteEnd() {
+    if(head == NULL || head->next == NULL){
+        return 0;
+    } else {
+        Node* curr = head;
+        Node* prev = head;
+        while(curr->next!=NULL) {
+            prev = curr;
+            curr = curr->next;
+        }
+        int res = curr->data;
+        prev->next = NULL;
+        delete curr;
+        return res;
+    }
+}
+
+int Linkedlist :: deleteAfter(Node* prev) {
+    if(prev != NULL) {
+        Node* temp = prev->next;
+        prev->next = temp->next;
+        int res = temp->data;
+        delete temp;
+        return res;
+    } else return 0;
+}
+
+void Linkedlist :: printList() {
+    Node* curr = head;
+    cout<<"\nThe list contains: ";
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+void Linkedlist :: reverseList(){
+    Node *prev = NULL, *curr = head, *next = NULL;
+    while(curr!=NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    
+}
+
+void Linkedlist :: reverseinK(Node* head, int k) {
+    if(head!=NULL){
+        Node *curr = head, *next = NULL, *prev = NULL;
+        int count =0;
+        while(curr!=NULL && count<k){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        if(next != NULL){
+            head->next = reverse(next, k);
+        }
+    }
+}
+
+bool Linkedlist :: cycledetection() {
+    Node *slow=head, *fast = head;
+    while(slow && fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast) return 1;
+    }
+    return 0;
+}
+
+int main() {
+    Linkedlist list;
+    list.insertBegin(10);
+    list.insertEnd(20);
+    list.insertBegin(5);
+    list.insertEnd(30);
+    list.printList();
+    list.deleteBegin();
+    list.deleteEnd();
+    list.printList();
+    list.reverseList();
+    list.printList();
+    
+    return 0;
+}
